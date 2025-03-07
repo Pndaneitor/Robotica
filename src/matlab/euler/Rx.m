cce729c6b85413a4396b5645081ed3ed8bb79527
DESKTOP-NOKK634\efris <efr>
2025-03-07 09:04:50 -0700

    Se añadio el matlab de euler asi como tambièn Ejercicios Denavit Hartenberg

--- src/matlab/euler/Rx.m
+++ src/matlab/euler/Rx.m
@@ -1,4 +1,6 @@
-function R = Rx(theta)
+function R=Rx(phi)
 %Rx realiza una rotación en un ángulo theta en radianes con respecto al eje X
-
-R =
+ R = [1,      0,       0;
+          0, cos(phi), -sin(phi);
+          0, sin(phi),  cos(phi)];
+end
--- src/matlab/euler/Rz.m
+++ src/matlab/euler/Rz.m
@@ -1,4 +1,6 @@
-function R = Rz(theta)
+function R = Rz(spi)
 %Rz realiza una rotación en un ángulo theta en radianes con respecto al eje z
 
-R =
+R =[cos(spi), -sin(spi), 0;
+    sin(spi),  cos(spi), 0;
+     0,          0,          1];
